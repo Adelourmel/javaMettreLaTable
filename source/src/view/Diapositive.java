6316474c35123ba124ecaa30fd317ead971c8e5f
Cayril <aymeric.bizouarn@hotmail.fr>
2019-05-27 21:52:17 +0200

    diapositive

--- source/src/view/Diapositive.java
+++ source/src/view/Diapositive.java
@@ -1,14 +1,23 @@
 package view;
 
 import control.DiapositiveListener;
+
 import javax.swing.JPanel;
 
-public class Diapositive extends JPanel{
+public class Diapositive extends JPanel {
     private int index;
+    private int nbSlide;
+    private String file;
+
     public Diapositive() {
         super();
     }
-    public Diapositive(int nbSlide,String title) {
+
+    public Diapositive(int nbSlide, String title) {
         super();
+        this.nbSlide = nbSlide;
+        this.title = title;
+        this.index = 0;
     }
+
 }
