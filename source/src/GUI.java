26f9190b97f918af37ec00d46bf31cc27e83f54f
Adelourmel <a.delourmel35@gmail.com>
2019-05-27 17:00:28 +0200

    Un début

--- source/src/GUI.java
+++ source/src/GUI.java
@@ -7,14 +7,22 @@ public class GUI extends JFrame {
 
   public GUI() {
     super("Titre ?");
+    
+    this.diapo = new Diapositive();
+    this.menu = new Menu();
 
+    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+    pack();
+    setVisible(true);
   }
 
 /**
  * Allow to show the menu
  */
   public void showMenu() {
-
+    add(this.menu);
+    remove(this.diapo);
   }
 
 /**
@@ -22,6 +30,7 @@ public class GUI extends JFrame {
  * @param nbSlide The slide number you want to show
  */
   public void showDiapo(int numSlide) {
-
+    add(this.diapo);
+    remove(this.menu);
   }
 }
--- source/src/Launcher.java
+++ source/src/Launcher.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 
 public class Launcher {
   public static void main(String[] args) {
-    Menu menu = new Menu();
+    GUI gui = new GUI();
   }
 }
