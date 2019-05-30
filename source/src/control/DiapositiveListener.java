f469543d8c6c9f1d1e66772b183be8d2b6cc2e23
Cayril <aymeric.bizouarn@hotmail.fr>
2019-05-30 12:28:49 +0200

    test

--- source/src/control/DiapositiveListener.java
+++ source/src/control/DiapositiveListener.java
@@ -1,7 +1,17 @@
 package control;
 
-public class DiapositiveListener {
-  public DiapositiveListener() {
-    
+import view.Diapositive;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DiapositiveListener extends ActionListener {
+  private Diapositive diapositive;
+  public DiapositiveListener(diapositive) {
+    this.diapositive = diapositive;
+  }
+
+  public void actionPerformed(ActionEvent e) {
+
   }
 }
--- source/src/control/MenuListener.java
+++ source/src/control/MenuListener.java
@@ -1,7 +1,15 @@
 package control;
 
-public class MenuListener {
-  public MenuListener() {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuListener extends ActionListener {
+  private Menu menu;
+  public MenuListener(menu) {
+    this.menu = menu;
+  }
+
+  public void actionPerformed(ActionEvent e) {
+
   }
 }
