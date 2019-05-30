05a9146e5c52757f8d4400008ac1482a00f2f392
Adelourmel <a.delourmel35@gmail.com>
2019-05-30 14:16:40 +0200

    Debut chrono

--- source/src/model/Chrono.java
+++ source/src/model/Chrono.java
@@ -1,7 +1,45 @@
 package model;
 
 public class Chrono {
+
+  private long timeInit;
+  private long finalTime;
+
   public Chrono() {
-    
+    this.timeInit = 0;
+    this.finalTime = 0;
+  }
+
+  public void start() {
+    this.timeInit = System.currentTimeMillis();
+  }
+
+  public void stop() {
+    this.finalTime = System.currentTimeMillis() - this.timeInit;
+  }
+
+
+  public void saveIfItBest(long newTime) {
+
+  }
+
+
+  public String beautify() {
+    String texteSecondes;
+    String texteMinutes;
+
+    if (this.secondes < 10) {
+      texteSecondes = "0" + this.secondes;
+    }
+    else {
+      texteSecondes = "" + this.secondes;
+    }
+    if (this.minutes < 10) {
+      texteMinutes = "0" + this.minutes;
+    }
+    else {
+      texteMinutes = "" + this.minutes;
+    }
+    return texteMinutes + ":" + texteSecondes;
   }
 }
---
+++ source/src/test/TestChrono.java
@@ -0,0 +1,10 @@
+public class TestChrono {
+  public static void main(String[] args) {
+
+    System.out.println("TESt chrono");
+    Chrono test = new Chrono();
+    test.start();
+
+    test.stop();
+  }
+}
