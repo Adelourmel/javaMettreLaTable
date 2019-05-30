c24012eab0f50f5a908c8718235e89161136d036
Adelourmel <a.delourmel35@gmail.com>
2019-05-30 15:03:13 +0200

    Fin chrono

--- source/src/model/Chrono.java
+++ source/src/model/Chrono.java
@@ -1,45 +1,85 @@
 package model;
 
+import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Chrono {
 
   private long timeInit;
-  private long finalTime;
+  private int finalTime;
+  private String fileName;
 
-  public Chrono() {
+  public Chrono(String fileName) {
     this.timeInit = 0;
     this.finalTime = 0;
+    this.fileName = fileName;
   }
 
   public void start() {
     this.timeInit = System.currentTimeMillis();
   }
 
-  public void stop() {
-    this.finalTime = System.currentTimeMillis() - this.timeInit;
+  public int stop() {
+    this.finalTime = (int) (System.currentTimeMillis() - this.timeInit);
+    System.out.println("Time : " +finalTime);
+    return saveIfItBest();
   }
 
 
-  public void saveIfItBest(long newTime) {
+  private int saveIfItBest() {
+
+    int score = -1;
+
+    try {
+      DataInputStream in = new DataInputStream(new FileInputStream(this.fileName));
+
+
+
+      score = in.readInt();
+
+
+      in.close();
+
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      writeInFile();
+
+    }
+    if (score != -1) {
+      if (score > this.finalTime) {
+        score = -1;
+        writeInFile();
+      }
+    }
+
+    return score;
+
 
   }
 
+  private void writeInFile() {
+    try {
+      DataOutputStream out = new DataOutputStream(new FileOutputStream(this.fileName));
 
-  public String beautify() {
-    String texteSecondes;
-    String texteMinutes;
 
-    if (this.secondes < 10) {
-      texteSecondes = "0" + this.secondes;
+      out.writeInt(this.finalTime);
+
+
+      out.close();
+
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
     }
-    else {
-      texteSecondes = "" + this.secondes;
-    }
-    if (this.minutes < 10) {
-      texteMinutes = "0" + this.minutes;
-    }
-    else {
-      texteMinutes = "" + this.minutes;
-    }
-    return texteMinutes + ":" + texteSecondes;
+  }
+
+
+  public String beautify(int time) {
+
+    return new SimpleDateFormat("mm:ss:SSS").format(new Date(time));
+  }
+
+  public int getFinalTime() {
+    return this.finalTime;
   }
 }
--- source/src/test/TestChrono.java
+++ source/src/test/TestChrono.java
@@ -1,10 +1,26 @@
+package test;
+
+import model.Chrono;
+
 public class TestChrono {
   public static void main(String[] args) {
 
     System.out.println("TESt chrono");
-    Chrono test = new Chrono();
+    Chrono test = new Chrono("../data/test.bin");
     test.start();
+    long i = -1000000000;
+    while (i < 1000000000) {
+      System.out.println("yop");
+      i++;
+    }
 
-    test.stop();
+    int tmp = test.stop();
+
+    if (tmp == -1) {
+      System.out.println("nouveau record : " + test.beautify(test.getFinalTime()));
+    }
+    else {
+      System.out.println(test.beautify(test.getFinalTime()) + " votre record est : " + test.beautify(tmp));
+    }
   }
 }
