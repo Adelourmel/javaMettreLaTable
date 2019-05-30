ec01c2054a75dcacee2d2be4cf4c62c41400651b
Adelourmel <a.delourmel35@gmail.com>
2019-05-30 17:24:38 +0200

    Debut menu

--- source/src/view/Menu.java
+++ source/src/view/Menu.java
@@ -1,30 +1,52 @@
 package view;
 
 import control.MenuListener;
+import model.Chrono;
 
 import javax.swing.*;
-import java.awt.BoardLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 public class Menu extends JPanel {
 
   private MenuListener listener;
+  private JButton buttonMettreTable;
 
   public Menu(Chrono chrono) {
     super();
 
     this.listener = new MenuListener(this);
 
-    this.setLayout(new BoardLayout());
+    this.setLayout(new GridLayout(3, 1));
 
-    createChoiceBox(chrono)
+    createChoiceBox(chrono);
   }
 
   public void createChoiceBox(Chrono chrono) {
 
-    createRow("Mettre la table", chrono.getBestTime());
-    createRow("Préparer un repas complet", chrono)
+    String temps;
+    if (chrono.getBestTime() == -1) {
+      temps = "0";
+    }
+    else {
+      temps = chrono.beautify(chrono.getBestTime());
+    }
+
+    this.buttonMettreTable = createRow("Mettre la table", temps);
+    createRow("Préparer un repas complet", "0").setEnabled(false);
+    createRow("Débarraser la table", "0").setEnabled(false);
   }
-  public JButton createRow(String title, int scores) {
+  public JButton createRow(String title, String temps) {
+    JPanel tmp = new JPanel();
+    tmp.setLayout(new BorderLayout());
+    JButton button = new JButton(title);
+    JLabel text = new JLabel("Meilleur temps : " + temps + "mn");
+    tmp.add(button, BorderLayout.CENTER);
+    tmp.add(button, BorderLayout.EAST);
+
+    this.add(tmp);
+
+    return button;
 
   }
 }
