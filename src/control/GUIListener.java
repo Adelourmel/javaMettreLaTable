e0354ea2636e89de60c4dd43ecd1cee06e55a102
Cayril <aymeric.bizouarn@hotmail.fr>
2019-06-06 09:18:31 +0200

    GUIListener javadoc

--- src/control/GUIListener.java
+++ src/control/GUIListener.java
@@ -5,14 +5,27 @@ import view.GUI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Listening Action in the GUI panel.
+ */
 public class GUIListener implements ActionListener {
-
+    /**
+     * The Gui window listen.
+     */
     private GUI gui;
 
+    /**
+     * Initialise the GUIListener.
+     * @param gui The GUIListener.
+     */
     public GUIListener(GUI gui) {
         this.gui = gui;
     }
 
+    /**
+     * Click on object detection.
+     * @param e The ActionEvent.
+     */
     public void actionPerformed(ActionEvent e) {
         if (this.gui.getTitle().contains("Menu")) {
             this.gui.dispose();
